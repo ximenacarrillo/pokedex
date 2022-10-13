643ec12f3028ddc0fc788d16ff323ba818f8f2e3
Jair Fonseca <jairfonseca64@yahoo.es>
2022-10-13 17:51:33 -0400

    Files missed

---
+++ app/src/main/java/com/isi/pokedex_xc_hf/models/Ability.java
@@ -0,0 +1,34 @@
+package com.isi.pokedex_xc_hf.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Ability {
+    @SerializedName("ability")
+    private AbilityName ability;
+    private boolean is_hidden;
+    private int slot;
+
+    public AbilityName getAbility() {
+        return ability;
+    }
+
+    public void setAbility(AbilityName ability) {
+        this.ability = ability;
+    }
+
+    public boolean isIs_hidden() {
+        return is_hidden;
+    }
+
+    public void setIs_hidden(boolean is_hidden) {
+        this.is_hidden = is_hidden;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public void setSlot(int slot) {
+        this.slot = slot;
+    }
+}
